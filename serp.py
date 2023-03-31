bd8eb9eb2adb7351cc840fb464894347b225aacd
grimg0d <128413844+grimg0d@users.noreply.github.com>
2023-03-31 07:04:57 +0530

    Update serp.py

--- serp.py
+++ serp.py
@@ -1,29 +1,58 @@
 import turtle
-def draw_sierpinski(length,depth):
-    if depth==0:
-        for i in range(0,3):
-            t.fd(length)
-            t.left(120)
-    else:
-        draw_sierpinski(length/2,depth-1)
-        t.fd(length/2)
-        draw_sierpinski(length/2,depth-1)
-        t.bk(length/2)
-        t.left(60)
-        t.fd(length/2)
-        t.right(60)
-        draw_sierpinski(length/2,depth-1)
-        t.left(60)
-        t.bk(length/2)
-        t.right(60)
+
+iterations = 3
+
+axiom = "F-G-G"
+rule_set = {
+    "F" : "F-G+F+G-F",
+    "G" : "GG"
+}
+angle = 120
+distance = 50
 
 
-window = turtle.Screen()
-try:
-    t = turtle.Turtle()
-except:
-    t = turtle.Turtle()
-t.speed(100)
-draw_sierpinski(100,3)
-window.exitonclick()
-turtle.mainloop()
+def applyRules(var, rule_set):
+    return var if (x := rule_set.get(var, None)) is None else x
+
+
+def processString(oldRule, rule_set):
+    newstr = ""
+    for ch in oldRule:
+        newstr = newstr + applyRules(ch, rule_set)
+    return newstr
+
+
+def createLSystem(numIters, axiom, rule_set):
+    startString = axiom
+    endString = ""
+    for i in range(numIters):
+        endString = processString(startString, rule_set)
+        startString = endString
+    return endString
+
+
+def drawLsystem(aTurtle, instructions, angle, distance):
+    for cmd in instructions:
+        if cmd == 'F'or cmd == "G":
+            aTurtle.forward(distance)
+        elif cmd == 'B':
+            aTurtle.backward(distance)
+        elif cmd == '+':
+            aTurtle.right(angle)
+        elif cmd == '-':
+            aTurtle.left(angle)
+
+
+inst = createLSystem(iterations, axiom, rule_set)
+print(inst)
+
+t = turtle.Turtle()
+wn = turtle.Screen()
+
+t.ht()
+t.up()
+t.back(200)
+t.down()
+t.speed(9)
+drawLsystem(t, inst, angle, distance)
+wn.exitonclick()
